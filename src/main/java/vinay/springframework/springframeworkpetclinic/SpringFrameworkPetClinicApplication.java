3d32e693f88dba7ce6c507f1681906746d70f25c
kanike-vinay <43097964+kanike-vinay@users.noreply.github.com>
2019-05-08 18:38:50 +0530

    Spring Framework Pet Clinic Initial Commit
    Signed-off-by: kanike-vinay <kanike.vinay@gmail.com>

---
+++ src/main/java/vinay/springframework/springframeworkpetclinic/SpringFrameworkPetClinicApplication.java
@@ -0,0 +1,13 @@
+package vinay.springframework.springframeworkpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameworkPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vinay/springframework/springframeworkpetclinic/SpringFrameworkPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package vinay.springframework.springframeworkpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringFrameworkPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
