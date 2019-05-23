e8893594bfe968ebb3b4dc34c62f7ad77dedbd81
kanike-vinay <kanike.vinay@gmail.com>
2019-05-23 07:52:13 +0530

    Vuetify demo

---
+++ meal-prep/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <span>
+        <home-hero></home-hero>
+        <home-details></home-details>
+        <home-plans></home-plans>
+    </span>
+</template>
+
+<script>
+import HomeHero from '@/components/HomeHero';
+import HomeDetails from '@/components/HomeDetails';
+import HomePlans from '@/components/HomePlans';
+
+export default {
+    name: 'home',
+    components: {
+        HomeHero,
+        HomeDetails,
+        HomePlans
+    }
+};
+</script>
